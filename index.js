080ec146109729d6637bfcebe33cfb0f7e769884
euqueroalmasoficial-arch <euqueroalmas.oficial@gmail.com>
2026-05-25 22:43:57 -0300

    Update index.js

--- index.js
+++ index.js
@@ -61,8 +61,8 @@ async function enviarParaTodos() {
 }
 
 // Teste às 17h50 — depois mude para '0 6 * * *'
-cron.schedule('40 22 * * *', enviarParaTodos, {
+cron.schedule('45 22 * * *', enviarParaTodos, {
   timezone: 'America/Sao_Paulo'
 });
 
-console.log('Bot ativo — aguardando 22h40 de Brasília...');
+console.log('Bot ativo — aguardando 22h45 de Brasília...');
